4102866b5429be6acfb3fff843f592ef3ae7c73a
Josph Simon <kakabrather@gmail.com>
2022-12-15 23:15:08 +0300

    Update 4-print_float.py

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
-number = 3.14159
-print("Float: {:.2f}".format(number))
+# Include the decimal module to use the Decimal type
+from decimal import Decimal
+
+# Define a float value
+x = 3.14159265
+
+# Use the format method to print the float with 2 decimal places
+print("{:.2f}".format(x))
+
+# Use the Decimal type to print the float with 4 decimal places
+print("{:.4f}".format(Decimal(x)))
