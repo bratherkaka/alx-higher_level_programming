9933887b35e5abb6c1233ccbd63c39ea76b30648
Josph Simon <kakabrather@gmail.com>
2022-12-20 16:07:46 +0300

    Add files via upload

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        count = 0
-        while count < x:
-            print("{}".format(my_list[count]), end='')
-            count += 1
-        print()
-        return count
-    except IndexError:
-        print()
-        return count
-        pass
+    count = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            count = count + 1
+        except IndexError:
+            continue
+    print("")
+    return count
