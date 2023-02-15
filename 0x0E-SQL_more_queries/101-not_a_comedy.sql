833113696af6153de2deb023effa3bf29a16b63f
Josph Simon <kakabrather@gmail.com>
2023-02-16 01:08:18 +0300

    Update 101-not_a_comedy.sql

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,5 +1,4 @@
 -- This script lists all shows without the genre Comedy in the hbtn_0d_tvshows database
-
 SELECT tv_shows.title
 FROM tv_shows
 WHERE tv_shows.id NOT IN
@@ -8,4 +7,3 @@ FROM tv_show_genres
 JOIN tv_genres ON tv_genres.id = tv_show_genres.tv_genre_id
 WHERE tv_genres.name = 'Comedy')
 ORDER BY tv_shows.title ASC;
-
