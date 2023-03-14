0ed3efca696944b84b668b4491ebdcf06aad7eb5
Josph Simon <kakabrather@gmail.com>
2023-03-14 11:58:04 +0300

    Update 103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,16 +1,19 @@
 #!/usr/bin/node
-
 const myObject = {
   type: 'object',
   value: 12,
-  incr: function() {
+  incr: function () {
     this.value++;
   }
 };
+
 console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
