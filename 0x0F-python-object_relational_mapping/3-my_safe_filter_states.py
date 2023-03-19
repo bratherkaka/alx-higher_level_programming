aa13da00d742592321a1533899364119504b6f6b
Josph Simon <kakabrather@gmail.com>
2023-03-19 21:30:20 +0300

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,16 +1,35 @@
 #!/usr/bin/python3
-# Displays all values in the states table of the database hbtn_0e_0_usa
-# whose name matches that supplied as argument.
-# Safe from SQL injections.
-# Usage: ./3-my_safe_filter_states.py <mysql username> \
-#                                     <mysql password> \
-#                                     <database name> \
-#                                     <state name searched>
-import sys
+"""
+This script displays all values in the states table of hbtn_0e_0_usa where
+name matches the argument. It is safe from SQL injections.
+"""
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
+    if len(sys.argv) == 5:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        db_name = sys.argv[3]
+        state_name = sys.argv[4]
+        conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=db_name,
+            charset="utf8"
+        )
+        cur = conn.cursor()
+        cur.execute(
+            "SELECT * FROM states WHERE name=%s ORDER BY id ASC",
+            (state_name,)
+        )
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        conn.close()
+    else:
+        print("Usage: {:s} username password database_name state_name".format(
+            sys.argv[0]))
