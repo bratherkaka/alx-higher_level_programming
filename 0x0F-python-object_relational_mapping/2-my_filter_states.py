019aaed22f3a058ae4e5d9ef0d832e9e933855f4
Josph Simon <kakabrather@gmail.com>
2023-03-19 21:28:27 +0300

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python3
-"""
-Script that takes in an argument and displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument.
-"""
+"""Module for filtering states by name"""
 
-import MySQLdb
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    # Take in four arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    db_name = sys.argv[3]
+    if len(sys.argv) != 5:
+        print("Usage: {} username password database_name state_name".format(
+            sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
     state_name = sys.argv[4]
 
-    # Connect to MySQL server running on localhost at port 3306
-    conn = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
-                           passwd=mysql_password, db=db_name, charset="utf8")
-    cur = conn.cursor()
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database_name)
 
-    # Execute SQL query to retrieve values
-    cur.execute("SELECT * FROM states WHERE name=%s ORDER BY id ASC",
-                (state_name,))
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+                .format(state_name))
+
     rows = cur.fetchall()
-
-    # Display results as they are in the example
     for row in rows:
         print(row)
 
-    # Close all cursors and database connections
     cur.close()
-    conn.close()
+    db.close()
