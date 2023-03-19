16aa03f0401bee1ba6e6b372b20a5f446af1294a
Josph Simon <kakabrather@gmail.com>
2023-03-19 21:42:08 +0300

    Update model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""Defines State class that maps to MySQL table states
+"""
+Defines the State class and a Base instance for ORM.
 """
 
-from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
 class State(Base):
-    """State class that maps to MySQL table states
+    """
+    State class that links to the MySQL table states.
     """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
+
