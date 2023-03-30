c8b330cb4ac3386ab9dfa043c24815d9ca705f8e
Josph Simon <kakabrather@gmail.com>
2023-03-30 12:00:13 +0300

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get the response body for a given URL for 200 status code responses.
-curl -sL "$1"
+# This script takes in a URL, sends a GET request to the URL, and displays the body of the response
+url="$1"; curl -sL -X GET "$url"
